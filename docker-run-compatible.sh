5bdefcfdb2054c234dbeb69a4f90ad9e174801a1
Jahwan Oh <jahwan.oh@bepro11.com>
2025-05-07 13:00:57 +0100

    docker buuild

--- docker-run-compatible.sh
+++ docker-run-compatible.sh
@@ -2,6 +2,7 @@
 
 # Run the Docker container with compatible TensorFlow
 docker run --gpus all -it \
+ -v $(pwd):/comisr \ 
   -v $(pwd)/data:/app/data \
   -v $(pwd)/model:/app/model \
   video-pipeline-sr-compatible 
