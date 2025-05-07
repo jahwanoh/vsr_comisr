b3255532f588b19a10dee1d27637d987658fc72a
Jahwan Oh <jahwan.oh@bepro11.com>
2025-05-07 14:08:24 +0100

    run option

--- gpu-run.sh
+++ gpu-run.sh
@@ -2,7 +2,7 @@
 
 # Run the Docker container with GPU support
 docker run --gpus all -it --rm \
-  -v $(pwd)/comisr:/comisr \
+  -v $(pwd):/comisr \
   -v $(pwd)/data:/app/data \
   -v $(pwd)/model:/app/model \
   video-pipeline-sr-gpu
